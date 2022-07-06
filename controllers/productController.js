c1e775ec1922a6f733b7abaa8003a69f81571807
Cesar Rodriguez <cesarrdgz.18.95@gmail.com>
2022-07-05 23:24:05 -0500

    get all products api function

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,17 @@
 const Product = require('../models/productModel');
 
+exports.getAllProducts = async (req, res, next) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    status: "success",
+    results: products.length,
+    data: {
+      products
+    }
+  });
+};
+
 exports.createProduct = async (req, res, next) => {
   const newProduct = await Product.create(req.body);
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,6 +4,8 @@ const productController = require('../controllers/productController');
 const router = express.Router();
 
 router
-  .route('/').post(productController.createProduct);
+  .route('/')
+    .get(productController.getAllProducts)
+    .post(productController.createProduct);
 
 module.exports = router;
